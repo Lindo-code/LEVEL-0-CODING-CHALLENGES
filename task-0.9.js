d33a6918edff039739321431b5eba3f0de55cb57
Lindo-code <sdrowvieli@gmail.com>
2021-07-16 15:29:05 +0200

    fixed duplicate vowels issue in task-0.9.js file and replaced return with console.log.

--- task-0.9.js
+++ task-0.9.js
@@ -3,13 +3,18 @@ function vowels(word1) {
   x = word1.length;
   var vowels = "aeiou";
   var word = "";
+  var letterDump = "";
   for (i = 0;i < x; i++ ){
     var character = word1[i]
-    if (vowels.indexOf(character) >= 0)
-    word += character + ", ";
+    if (vowels.indexOf(character) >= 0){
+      if (word.indexOf(character) >= 0){
+        letterDump = character;
+      } else {
+        word += character + ", ";
+      }
+    }
   }
   var words = word.slice(0, -2);
   words = "Vowels: " + words;
-  return words;
+  console.log(words);
 }
-
