20a1005a2b4b5a7f509982eb3f4de54051421a66
Lindo-code <sdrowvieli1@gmail.com>
2021-11-20 11:48:31 +0200

    removed maxAnyNum function, used arguments object to retrieve parameters.

--- task-0.6.js
+++ task-0.6.js
@@ -1,23 +1,10 @@
-function maximumNum(num1, num2, num3){
-  var maxNum = ''
-  if (num1 > num2 && num1 > num3) {
-    maxNum = num1;
-  } else if (num2 > num1 && num2 > num3) {
-    maxNum = num2;
-  } else {
-    maxNum = num3;
-  }
-  return maxNum;
-}
-function maximumAnyNum(...nums){
-  var nums = [...nums];
-  var x = nums.length;
-  var largestNum = nums[0];
-  for (var i = 0; i < x; i++){
-    if (largestNum < nums[i]){
-      largestNum = nums[i];
-    }
+function maximumNum(){
+  let largestNum = 0;
+  for(let i = 0; i < arguments.length; i++){
+    if(largestNum < arguments[i]){
+      largestNum = arguments[i];
+    };
   }
   return largestNum;
 }
-
+console.log(maximumNum(1, 49, 6, 8, 90));
