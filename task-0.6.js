b0d9ac7795164d506316473d2a6afece172f0cbf
Lindo-code <sdrowvieli1@gmail.com>
2021-11-22 16:21:38 +0200

    added conditons to process negative numbers.

--- task-0.6.js
+++ task-0.6.js
@@ -1,6 +1,11 @@
 function maximumNum(){
   let largestNum = 0;
   for(let i = 0; i < arguments.length; i++){
+    if(i === 0) {
+      if(arguments[i] < 0) {
+        largestNum = arguments[i];
+      };
+    }
     if(largestNum < arguments[i]){
       largestNum = arguments[i];
     };
